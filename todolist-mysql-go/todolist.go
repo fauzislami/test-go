62b9ce8d8d3311367a9fe8040fd399eeb148902a
fauzislami <islamifauzi@gmail.com>
2022-09-02 14:10:25 +0700

    test changes

--- todolist-mysql-go/todolist.go
+++ todolist-mysql-go/todolist.go
@@ -1,25 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
-	"github.com/gorilla/mux"
-	log "github.com/sirupsen/logrus"
+	"strconv"
+
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"encoding/json"
-	"strconv"
 	"github.com/rs/cors"
+	log "github.com/sirupsen/logrus"
 )
-
 
 var db, _ = gorm.Open("mysql", "root:root@/todolist?charset=utf8&parseTime=True&loc=Local")
 
-type TodoItemModel struct{
-	Id int `gorm:"primary_key"`
+type TodoItemModel struct {
+	Id          int `gorm:"primary_key"`
 	Description string
-	Completed bool
+	Completed   bool
 }
 
 func CreateItem(w http.ResponseWriter, r *http.Request) {
@@ -33,15 +33,15 @@ func CreateItem(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateItem(w http.ResponseWriter, r *http.Request) {
-        // Get URL parameter from mux
-        vars := mux.Vars(r)
-        id, _ := strconv.Atoi(vars["id"])
+	// Get URL parameter from mux
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
 
-        // Test if the TodoItem exist in DB
-        err := GetItemByID(id)
-        if err == false {
+	// Test if the TodoItem exist in DB
+	err := GetItemByID(id)
+	if err == false {
 		w.Header().Set("Content-Type", "application/json")
-                io.WriteString(w, `{"updated": false, "error": "Record Not Found"}`)
+		io.WriteString(w, `{"updated": false, "error": "Record Not Found"}`)
 	} else {
 		completed, _ := strconv.ParseBool(r.FormValue("completed"))
 		log.WithFields(log.Fields{"Id": id, "Completed": completed}).Info("Updating TodoItem")
@@ -50,7 +50,7 @@ func UpdateItem(w http.ResponseWriter, r *http.Request) {
 		todo.Completed = completed
 		db.Save(&todo)
 		w.Header().Set("Content-Type", "application/json")
-                io.WriteString(w, `{"updated": true}`)
+		io.WriteString(w, `{"updated": true}`)
 	}
 }
 
@@ -70,14 +70,14 @@ func DeleteItem(w http.ResponseWriter, r *http.Request) {
 		db.First(&todo, id)
 		db.Delete(&todo)
 		w.Header().Set("Content-Type", "application/json")
-                io.WriteString(w, `{"deleted": true}`)
+		io.WriteString(w, `{"deleted": true}`)
 	}
 }
 
 func GetItemByID(Id int) bool {
 	todo := &TodoItemModel{}
 	result := db.First(&todo, Id)
-	if result.Error != nil{
+	if result.Error != nil {
 		log.Warn("TodoItem not found in database")
 		return false
 	}
@@ -131,8 +131,8 @@ func main() {
 	router.HandleFunc("/todo/{id}", DeleteItem).Methods("DELETE")
 
 	handler := cors.New(cors.Options{
-                AllowedMethods: []string{"GET", "POST", "DELETE", "PATCH", "OPTIONS"},
-        }).Handler(router)
+		AllowedMethods: []string{"GET", "POST", "DELETE", "PATCH", "OPTIONS"},
+	}).Handler(router)
 
 	http.ListenAndServe(":8000", handler)
 }
